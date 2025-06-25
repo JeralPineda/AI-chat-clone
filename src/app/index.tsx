51bf0d3e0622cd991b16256cfec0052100a0cb9d
Jeral Pineda <jeral.cris@gmail.com>
2025-06-24 20:54:00 -0600

    fix: setup expo router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,8 @@
-import "./global.css";
-
 import { ListItem } from "@/components/list-item";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
-export default function App() {
+export default function HomeScreen() {
   return (
     <View className="bg-white dark:bg-black flex-1 justify-center items-center">
       <Text className="text-dark dark:text-white text-3xl font-semibold">Hello World</Text>
