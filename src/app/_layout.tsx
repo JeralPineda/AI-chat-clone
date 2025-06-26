cdb9c1a5528f05495e8ad83e5a34f1c3715473e4
Jeral Pineda <jeral.cris@gmail.com>
2025-06-25 21:10:25 -0600

    fix: chat screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -48,9 +48,9 @@ export default function RootLayout() {
               },
             }}
           />
-
-          <StatusBar style="auto" />
         </Drawer>
+
+        <StatusBar style="light" />
       </ThemeProvider>
     </GestureHandlerRootView>
   );
--- src/app/chat/[id].tsx
+++ src/app/chat/[id].tsx
@@ -1,12 +1,23 @@
 import { useLocalSearchParams } from "expo-router";
 import { Text, View } from "react-native";
+import chatHistory from "@/assets/data/chatHistory.json";
 
 export default function ChatScreen() {
   const { id } = useLocalSearchParams();
 
+  const chat = chatHistory.find((chat) => chat.id === id);
+
+  if (!chat) {
+    return (
+      <View className="bg-black flex-1 justify-center items-center">
+        <Text className="text-white">Chat {id} not found</Text>
+      </View>
+    );
+  }
+
   return (
     <View className="bg-black flex-1">
-      <Text className="text-white">Chat Screen: {id}</Text>
+      <Text className="text-white">Chat Screen: {chat?.title}</Text>
     </View>
   );
 }
